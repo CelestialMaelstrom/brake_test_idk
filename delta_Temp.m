a6424cdc263901edb8379c20de5219eb0f598734
Oliver Ahern <oliveroliver.h.a@gmail.com>
2025-11-19 18:36:34 -0800

    b

--- delta_Temp.m
+++ delta_Temp.m
@@ -1,40 +1,71 @@
 function dT = delta_Temp(T_current, vel, accel, params, dt)
 
     %-----------------------------------
-    % 1. Brake Power Input From Decel
+    % 1. Calculate Forces (Newton's 2nd Law)
     %-----------------------------------
-    if accel < 0
-        P_brake = params.m_car * (-accel) * vel;   % watts
-    else
-        P_brake = 0;
-    end
+    % Total inertial force required to decelerate the car mass
+    % (Using -accel since decel is negative in data)
+    F_inertial = params.m_car * (-accel);
+
+    %-----------------------------------
+    % 2. Subtract "Free" Deceleration (Drag)
+    %-----------------------------------
+    % These forces slow the car but do NOT heat the brakes.
+    F_aero = 0.5 * params.rho_air * params.Cd * params.A_frontal * (vel^2);
+    F_roll = params.Crr * params.m_car * params.g;
     
-    % Fraction to rotor
-    p = params.e * params.S_r / (params.e * params.S_r + params.e * params.S_p);
-    H_d = p * P_brake;
-
+    % The actual force the tires/brakes must generate
+    F_brake_net = F_inertial - F_aero - F_roll;
 
     %-----------------------------------
-    % 2. Convection (use your table)
+    % 3. Brake Power Input
     %-----------------------------------
-    v_kmh = max(vel * 3.6, 0);  % m/s → km/h
+    if F_brake_net > 0
+        % Total braking power of the whole car
+        P_car_total = F_brake_net * vel; 
+        
+        % APPLY BIAS: 
+        % params.bias_front (0.63) goes to Front Axle
+        P_front_axle = P_car_total * params.bias_front;
+        
+        % SPLIT L/R: 
+        % 50% of axle power goes to THIS single rotor
+        P_rotor_input = P_front_axle / 2;
+        
+        % PARTITION: Heat split between Rotor and Pad
+        % FIX: Use distinct effusivity for Rotor (Steel) vs Pad
+        % Formula: p = (e_r * S_r) / (e_r * S_r + e_p * S_p)
+        numerator = params.e_rotor * params.S_r;
+        denominator = (params.e_rotor * params.S_r) + (params.e_pad * params.S_p);
+        
+        p = numerator / denominator;
+        
+        H_d = p * P_rotor_input;
+        
+    else
+        % Car is accelerating or coasting (drag > inertial force)
+        H_d = 0;
+    end
+
+    %-----------------------------------
+    % 4. Cooling (Convection + Radiation)
+    %-----------------------------------
+    % Convection (using your lookup table)
+    v_kmh = max(vel * 3.6, 0);
     h = interp1(params.h_vel, params.h_Wm2K, v_kmh, 'linear', 'extrap');
+    H_conv = h * params.A_rotor * (T_current - params.T_amb);
 
-    H_conv = h * params.A_rotor * max(T_current - params.T_amb, 0);
-
-    %-----------------------------------
-    % 3. Radiation
-    %-----------------------------------
+    % Radiation (Corrected to Kelvin)
     T_K = T_current + 273.15;
-    T_amb_K = 293.15;   % 20°C
-
+    T_amb_K = params.T_amb + 273.15;
+    
+    % Using corrected emissivity (0.55) from buildParams
     H_rad = params.eps * params.sigma * params.A_rotor * (T_K^4 - T_amb_K^4);
 
     %-----------------------------------
-    % 4. Rotor temperature change
+    % 5. Calculate dT
     %-----------------------------------
     dT_dt = (H_d - H_conv - H_rad) / (params.m_r * params.c_r);
-
     dT = dT_dt * dt;
 
 end
